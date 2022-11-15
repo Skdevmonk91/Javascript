af0f474a3201faa6090dd97b1fe855b8b5a0d504
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-15 14:34:14 +0530

    JavaScript Object

---
+++ JS Object.js
@@ -0,0 +1,13 @@
+//Created new object and added few values
+let human = {
+    firstName: 'Manav',
+    age: 15
+}
+
+//Dot notation(More easy method to implement use as default method)
+human.firstName = 'mihir'
+
+//Bracket notation(Use this method on more complicated cases) 
+human['firstName'] = '19'
+
+console.log(human);
