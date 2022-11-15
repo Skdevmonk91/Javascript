f76273e5c1c3fb5764ec55d13a47674f26ab834f
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-15 13:56:08 +0530

    JavaScript Array

---
+++ JS Array.js
@@ -0,0 +1,8 @@
+//array is consider as a object in javascript.
+let seven = []; //Initialize the empty array
+let selectedColors = [3, 'Arcade']; //Added new value in a function for output
+selectedColors[2] = 'Mihir'; //Add new value in same function because of dynamic size
+
+/* console.log(selectedColors); */
+//It have multiple properties to use in javascript one of them is-
+console.log(selectedColors.length); //length is use to check length of an array
