35b3e9e58a0a08f477d793cb2879c81a0487dd74
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-15 11:05:22 +0530

    JavaScript Primitive Type

---
+++ JS Primitive.js
@@ -0,0 +1,8 @@
+/*All of started with method called camel notation(Means any word started with
+lowercase and other every word started with uppercase)*/
+let name = 'KEVAL';//String(is a no of characters can be add)
+let no = 119;//Number(It has to be a number as value)
+let isApproved = false;//Boolean(it only supports either true or false)
+let lastName = undefined;//Undefined(use for define no value)
+let test = null;//Null(is a value for nothing or zero)
+let selectedColor = 'Green';
