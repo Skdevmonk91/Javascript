5ee9f5148dddf6a1db5693b478a3b21fd584a653
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-15 15:45:20 +0530

    JavaScript Function

---
+++ JS Function.js
@@ -0,0 +1,21 @@
+//Functions don't need semicolons
+
+
+/*function revise(){
+        . . .
+} Basic Function structure */
+
+//showing basic example of function
+//revise is an function and enlight is a parameter of that function
+//parameter is only work for a same function and not outside of function
+//Function can use more than one parameter
+/* parameter is add at time of declaration and argument is an actual
+value that use for the parameter */
+function revise(enlight, know) {
+    console.log('Namstey ' + enlight + ' ' + know)
+}
+
+//An argument can pass multiple time using same function
+//all values you should define in argument to get right output
+revise('India', 'From My Side');
+revise('Bharat', 'Meri Taraf Se');
