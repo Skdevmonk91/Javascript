94c35f2a37dcbe94af0960c7edd88c35b3f50246
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-15 16:59:17 +0530

    JavaScript Function 2

---
+++ JS Function 2.js
@@ -0,0 +1,13 @@
+//Using function ability creating a basic maths function for usecase
+//Added function named maths and used for testing basic equation
+function maths(number) {
+    return number * number - 33;
+}
+
+//declared with argument and input amount to do calculations
+//and show the output through window alert at first
+/* let number = maths(12);
+window.alert(number); */ //first way to show the output
+
+//We can directly show the output through window alert with argument function
+window.alert(maths(12)); 
