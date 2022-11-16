e3becad89b5e985717527889b4032be1a635160b
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-16 14:09:48 +0530

    JavaScript Comments

---
+++ JS Comments.js
@@ -0,0 +1,31 @@
+// Change heading:
+document.getElementById("myH").innerHTML = "JavaScript Comments";
+// Change paragraph:
+document.getElementById("myP").innerHTML = "New page to add the work and mandatory to last paid in the first page.";
+
+
+
+let a = 5;      // Declare x, give it the value of 5
+let b = a + 2;  // Declare y, give it the value of a + 2 
+
+
+
+// Write b to demo:
+document.getElementById("restart").innerHTML = b;
+
+
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+*/
+document.getElementById("rate").innerHTML = "You are being part of comment.";
+
+
+
+/*
+document.getElementById("work1").innerHTML = "Welcome to my Homepage";
+document.getElementById("work2").innerHTML = "This is my first paragraph.";
+*/
+document.getElementById("work2").innerHTML = "The comment-block is not executed.";
