eac7bb83856a6274cec2421271f7ea15b9844d9a
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-16 16:59:38 +0530

    JavaScript Operators

---
+++ JS Operators.js
@@ -0,0 +1,40 @@
+// Assign the value 5 to x
+let a = 5;
+// Assign the value 2 to y
+let b = 2;
+// Assign the value x + y to z:
+let c = a + b;
+
+//building a math operation for equation with pre-defined values
+let g = 9;
+let h = 8;
+let i = g * h;
+
+//create and use advance math function for complicated values(pre-defined values)
+let d = 84;
+let f = (100 + 50) * d;
+
+//Working on addition method in pre-defined value for the answer
+let k = 100;
+k += 55;
+
+//adding text in new structure for viewing the output
+let text1 = "Mihir";
+let text2 = "Menon";
+let text3 = text1 + " " + text2;
+
+let text4 = "Is It Dynamic ";
+text1 += "Working Ok";
+
+//Assigning a value for the final output
+let u = 184 + 33;
+let v = "50" + 65;
+let w = "Hello" + 70;
+
+//To show all upper method and value using console log
+console.log(c);
+console.log(i);
+console.log(k);
+console.log(text3);
+console.log(text4);
+console.log(u, v, w);
