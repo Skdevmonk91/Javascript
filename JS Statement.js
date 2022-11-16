7abc1f085d53d7e9eff9b565b46a4de325323612
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-16 11:58:55 +0530

    JavaScript Statements

---
+++ JS Statement.js
@@ -0,0 +1,16 @@
+//Working with different type of statements for example
+//Creating a new function for demonstration
+function myFunction() {
+    document.getElementById("demo3").innerHTML = "Hello Dolly!";
+    document.getElementById("demo4").innerHTML = "How are you?";
+}
+
+let x, y, z; //specifying x,y and z
+x = 5; //Giving x an value
+y = 6; //Giving y an value
+z = x + y; //Working with basic math functions
+
+
+
+window.alert(z); //Viewing final output through window alert
+console.log(myFunction); //Checking code of function in log 
