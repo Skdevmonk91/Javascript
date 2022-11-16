ffff66cda8a027d4e75fd32bb93e908d8b4f1547
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-16 12:59:22 +0530

    JavaScript Syntax

---
+++ JS Syntax.js
@@ -0,0 +1,42 @@
+// Create variables:
+var x;
+let y;
+
+// Use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+document.getElementById("demo").innerHTML = 10.50;
+
+
+
+document.getElementById("demo2").innerHTML = 'JASON DERULO';
+
+
+
+document.getElementById("demo3").innerHTML = (5 + 6) * 10;
+
+
+
+var a;
+a = 5;
+document.getElementById("demo4").innerHTML = a * 10;
+
+
+
+document.getElementById("demo5").innerHTML = "Raman" + " " + "Local";
+
+
+
+let d, e;
+d = 9 + 7;
+e = d * 10;
+document.getElementById("demo6").innerHTML = e;
+
+
+
+let k;
+k = 5;
+// k = 6; I will not be executed
+document.getElementById("demo7").innerHTML = k;
