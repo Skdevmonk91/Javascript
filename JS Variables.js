5b23388f4ad009b4f5df9196c274ad10b5f1dd10
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-17 16:58:54 +0530

    JavaScript Variables

---
+++ JS Variables.js
@@ -0,0 +1,37 @@
+//Live example of the variable in the JavaScript Variables
+var s = 45;
+var t = 60;
+var u = s + t;
+
+
+
+//The dollar sign is treated as a letter in JavaScript
+let $$$ = 9;
+let $myMoney = 187;
+
+
+
+//Underscore is treated as a letter in JavaScript names
+let _a = 200;
+let _100 = 50;
+
+
+
+//The result of addition is in stored and returned as output
+let b = 15 + 50 + 4;
+
+
+
+//Price1 , Price2 , and sum are variables stored total and let the output in on output page   
+const price1 = 55;
+const price2 = 40;
+let total = price1 + price2;
+
+
+
+//To show all the output of the variables and questions
+document.getElementById("ex1").innerHTML = $$$ + $myMoney;
+document.getElementById("ex2").innerHTML = _a + _100;
+document.getElementById("ex3").innerHTML = b;
+document.getElementById("price").innerHTML = "Total Price: " + total;
+document.getElementById("ask").innerHTML = "Value Of d Is: " + u;
