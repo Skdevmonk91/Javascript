fbbd9f2c298a21550dc197529648d7096eb739ff
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-17 14:58:13 +0530

    JavaScript Assignment Operator

---
+++ JS Assignment.js
@@ -0,0 +1,111 @@
+//Example Of Simple Operator In Assignment Operator
+let b = 35
+let a = 167 + b;
+
+
+
+//Real time example of addition assignment operator in assignment operator
+let c = 26;
+c += 12;
+
+let text = "Advance";
+text += " Coding";
+
+
+
+//New example of subtraction assignment operator in assignment operator
+let d = 100;
+d -= 31;
+
+
+
+//Example of multiplication assignment operator in assignment operator 
+let e = 30;
+e *= 15;
+
+
+
+//Example of exponentiation assignment operator in assignment operator  
+let f = 50;
+f **= 3;
+
+
+
+//Example of division assignment operator in assignment operator
+let g = 37;
+g /= 2;
+
+
+
+//Example of reminder assignment operator in assignment operator
+let h = 28;
+h %= 7;
+
+
+
+//Left-shift assignment operator in assignment operator
+let i = -140;
+i <<= 50;
+
+
+
+//Right-shift assignment operator in assignment operator
+let j = -300;
+j >>= 45;
+
+
+
+//Unsigned right-shift assignment operator in assignment operator
+let k = -150;
+k >>>= 14;
+
+
+
+//Bitwise And Assignment operator in assignment operator
+let l = 20;
+l &= 8;
+
+
+
+//Bitwise Or Assignment operator in assignment operator 
+let m = 70;
+m |= 15;
+
+
+
+//Bitwise XOR Assignment operator in assignment operator
+let n = 130;
+n ^= 35;
+
+
+
+//Logical AND Assignment operator in assignment operator
+let o = 90;
+o &&= 25;
+
+
+
+//Logical OR Assignment operator in assignment operator
+let p = undefined;
+p ||= 5;
+
+
+
+/*To show output of all assignment operators
+internal topics*/
+document.getElementById("ex1").innerHTML = "Value of a is: " + a;
+document.getElementById("ex2").innerHTML = "Value of c is: " + c;
+document.getElementById("ex3").innerHTML = text;
+document.getElementById("ex4").innerHTML = "Value of d is: " + d;
+document.getElementById("ex5").innerHTML = "Value of e is: " + e;
+document.getElementById("ex6").innerHTML = "Value of f is: " + f;
+document.getElementById("ex7").innerHTML = g;
+document.getElementById("ex8").innerHTML = "Value of h is: " + h;
+document.getElementById("ex9").innerHTML = "Value of i is: " + i;
+document.getElementById("eg1").innerHTML = "Value of j is: " + j;
+document.getElementById("eg2").innerHTML = "Value of k is: " + k;
+document.getElementById("eg3").innerHTML = "Value of l is: " + l;
+document.getElementById("eg4").innerHTML = "Value of m is: " + m;
+document.getElementById("eg5").innerHTML = "Value of n is: " + n;
+document.getElementById("eg6").innerHTML = "Value of o is: " + o;
+document.getElementById("eg7").innerHTML = "Value of p is: " + p;
