81c8299ce4d956c42a905dd7b9d18f2c87568839
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-22 12:59:11 +0530

    JavaScript Logical Operator

---
+++ Logical Operator.js
@@ -0,0 +1,28 @@
+//Logical Operators
+/*Operators is that look like given before sign to calculate the 
+value(ex =,-,/,etc..)*/
+/*Operands are the actually given value in question to give 
+the answer("a=30,b=26" here 30,26 is operands and = is operator*/
+
+//Logical And Operator
+/*If both conditions are true then it can print true otherwise
+it will print false(only applicable in logical and operator)*/
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+//Logical OR operator
+/*If one of the condition is true it will print true otherwise
+it will print false(only applicable in logical or operator)*/
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//Logical not
+/*It is used to print opposite of the text condition means if it has
+value true than it will print false and if has false value than it
+will print true(only applicable in logical not operator)*/
+console.log(!false);
+console.log(!true);
