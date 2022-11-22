ec739a6d4182989b2bf30e2480efead8ea662b79
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-22 13:00:08 +0530

    JavaScript Array 2

---
+++ JS Array 2.js
@@ -0,0 +1,7 @@
+/*Creating and demonstrating a basic array in new example using variable initializing and and set of array values inside of defined variable name */
+var arr1 = [1, 2, 3, 4, 5, "Jack", 6, 7, 8, 9, 'Code']
+
+//To get the output using console and log
+console.log(arr1);
+console.log(arr1[5]);
+console.log(arr1[10]);
