807f7fe968548348c5451f575019f2bb5f298ad4
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-28 17:10:49 +0530

    JavaScript Backticks

---
+++ JS Backticks.js
@@ -0,0 +1,7 @@
+//Template Literals - Backticks(`)
+//It used to replace text/item/anything from first one 
+/* first it will give outcome replace text and addition of original text like
+(${obj/item}) */
+//with help of these we don't have to make addition each time
+x = 900;
+document.write(`I have just changed text replace with normal ${x} `);
