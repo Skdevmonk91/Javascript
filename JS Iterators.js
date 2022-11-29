bf3e9e38113042ddaaf9ebedb0af7bf3b8b06661
Skdevmonk91 <keval.devmonk@gmail.com>
2022-11-29 15:16:44 +0530

    JavaScript Iterators

---
+++ JS Iterators.js
@@ -0,0 +1,29 @@
+//Iterator in JavaScript
+
+/* it is an array/string that we gonna show output one by one of internal
+print using different types of loops(for each,for of,etc...) */
+
+//In simple way check the output values one by one
+
+//Iterable Objects Are Array And String
+
+
+
+//Creating a object of demonstration
+let lang = ['Java', 'JavaScript', 'Python', 'Android', 'Bootstrap'];
+
+
+
+//Basic Iterator Using (For Of) Loop
+for (type of lang) {
+    console.log(type);
+}
+
+
+
+/* It is also possible without making any object and just
+for only one string or single words/word. */
+//Testing Single String Using Iterator Method In (For Of) Loop
+for (words of 'Demonstration') {
+    console.log(words);
+}
