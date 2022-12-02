76493847afd258634ad028b3ca2e2103f9c76c3c
Skdevmonk91 <keval.devmonk@gmail.com>
2022-12-02 13:49:19 +0530

    JavaScript While Loop

---
+++ While loop.js
@@ -0,0 +1,23 @@
+//JavaScript While Loop
+//while loop also implements like other loop with different structure
+// While loop runs first condition and then execute code until condition is true 
+
+
+
+//Creating a variable for while loop
+let n = prompt("Enter your value to print: ");
+
+
+//Making them to output convert into integer or only take integer
+n = Number.parseInt(n);
+
+
+//variable for to make condition variable
+let i = 0;
+
+
+//final while loop to execute and print integer
+while (i < n) {
+    console.log(i);
+    i++;
+}
