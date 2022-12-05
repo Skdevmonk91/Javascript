da311e06e072acae813fa2b9dd51921b08812df4
Skdevmonk91 <keval.devmonk@gmail.com>
2022-12-05 10:36:50 +0530

    JavaScript TypeOf

---
+++ JS Type of.js
@@ -0,0 +1,35 @@
+//Type of in javascript
+//typeof operator to find the data type of a JavaScript variable
+
+//Initializing different values for example 
+let a = "arcade";
+
+let b = 23;
+
+let c = false;
+
+let d = [21, "add"];
+
+let e = {
+    Name: "Mihir",
+    Position: "Fresher"
+};
+
+let f = function (m, n) {
+    i = m * n;
+}
+
+let g = null;
+
+let h = undefined;
+
+
+//to check the value of an Initialized
+console.log("Type of a is: ", typeof (a));
+console.log("Type of b is: ", typeof (b));
+console.log("Type of c is: ", typeof (c));
+console.log("Type of d is: ", typeof (d));
+console.log("Type of e is: ", typeof (e));
+console.log("Type of f is: ", typeof (f));
+console.log("Type of g is: ", typeof (g));
+console.log("Type of h is: ", typeof (h));
