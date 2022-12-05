38fa449d6b7072ad6a1d3fc5eb72d45d686fe899
Skdevmonk91 <keval.devmonk@gmail.com>
2022-12-05 13:39:08 +0530

    JavaScript Type coercion

---
+++ Type coercion.js
@@ -0,0 +1,24 @@
+//Type coercion
+
+
+
+/* We create number string addition to number but output will be
+will change number into string and join both string but it won't 
+make addition */
+
+let string1 = "300"; //String number initialized
+let number1 = 45; //creating an number value with
+
+console.log("Final Output Of Addition: ", string1 + number1) //For outcome
+
+
+
+
+/* But if we use number string convert into number and add 
+it with number and if we want make addition it will make
+addition and give the final outcome */
+
+let string2 = Number("300"); //String number initialized and converted into number
+let number2 = 125; //creating an number value
+
+console.log("Final Output Of Addition: ", string2 + number2) //For outcome
