3fa3314a29cbe9b7f7aa70c5b9e92dcefe1c0df0
Skdevmonk91 <keval.devmonk@gmail.com>
2022-12-06 12:05:30 +0530

    JavaScript Operator Precedence

---
+++ Operator Precedence.js
@@ -0,0 +1,7 @@
+//Javascript Operator Precedence
+/* Precedence is not an actual sentence or method but it decides rules for arithmetic expressions */
+
+/* FCFS(first come first served) means it will calculate values in line if there is addition and then division or any order that defined first it will calculate 
+in same order */
+document.getElementById("ex1").innerHTML = 150 + 250 * 3; //first calculation values
+document.getElementById("ex2").innerHTML = 10 * 45 / 3; //second calculation values
